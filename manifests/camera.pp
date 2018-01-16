042a9355392337f948a662bd37479d2dabdc9c85
Carl Caum <carl@carlcaum.com>
2018-01-16 00:11:45 -0600

    Do not manage homebridge

--- manifests/camera.pp
+++ manifests/camera.pp
@@ -1,9 +1,3 @@
 docker_compose { '/home/ccaum/homekit-person-alarm/docker-compose.yml':
   ensure  => present,
 }
-
-docker::run { 'homebridge':
-  image   => 'ccaum/homebridge-ffmpeg',
-  net     => 'host',
-  volumes => ['/home/ccaum/homebridge:/homebridge'],
-}
