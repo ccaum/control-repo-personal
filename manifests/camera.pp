6c280830d494f443b09c74d2ea0618207c2ce2f1
Carl Caum <carl@carlcaum.com>
2018-02-23 20:02:59 -0800

    Manage FTP recording destinations and Shinobi

--- manifests/camera.pp
+++ manifests/camera.pp
@@ -1,3 +1,13 @@
 docker_compose { '/home/ccaum/homekit-person-alarm/docker-compose.yml':
   ensure  => present,
 }
+
+## For FTP account
+user { 'garagecamera':
+  home  => '/media/GarageCamera',
+  shell => '/bin/bash',
+}
+
+docker_compose { '/media/GarageCamera/Shinobi/docker-compose.yml':
+  ensure => present,
+}
