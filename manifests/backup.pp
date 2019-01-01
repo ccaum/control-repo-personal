eb8406e98efd763cd0f0b0ab723a7cb2768bc4d6
Carl Caum <carl@carlcaum.com>
2019-01-01 12:17:03 -0800

    Backup the media and time machine disks

---
+++ manifests/backup.pp
@@ -0,0 +1,24 @@
+mounttab { "/mnt/backup":
+  ensure   => present,
+  device   => "UUID=66a1277a-4964-48e0-a64f-624f1bfc0d04",
+  fstype   => "xfs",
+  options  => ["rw"],
+  atboot   => "yes",
+  provider => augeas,
+}
+
+cron { 'backup-media':
+  ensure  => present,
+  command => 'rsync -av /mnt/media /mnt/backup/',
+  user    => root,
+  hour    => 9,
+  minute  => 0,
+}
+
+cron { 'backup-timemachine':
+  ensure  => present,
+  command => 'rsync -av /mnt/timemachine /mnt/backup/',
+  user    => 'root',
+  hour    => 9,
+  minute  => 0,
+}
