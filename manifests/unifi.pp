4e54cae9b4105da549e6cb6672ab1e5a4c46e70a
Carl Caum <carl@carlcaum.com>
2019-01-27 13:16:58 -0800

    Increase JVM stack to 2G for Unifi controller

--- manifests/unifi.pp
+++ manifests/unifi.pp
@@ -2,5 +2,6 @@ docker::run { 'unifi':
   image                 => 'jacobalberty/unifi:stable',
   net                   => 'host',
   volumes               => ['/home/ccaum/unifi:/unifi'],
+  env                   => [ "JVM_MAX_THREAD_STACK_SIZE=2048k" ],
   health_check_interval => 30,
 }
