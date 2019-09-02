69072ec9e5a5894116b1d955da1c0b6067d7820c
Carl Caum <carl@carlcaum.com>
2019-09-02 08:45:10 -0700

    Limit the unifi controller to 512mb of RAM

--- manifests/unifi.pp
+++ manifests/unifi.pp
@@ -2,6 +2,7 @@ docker::run { 'unifi':
   image                 => 'jacobalberty/unifi:stable',
   net                   => 'host',
   volumes               => ['/home/ccaum/unifi:/unifi'],
+  memory_limit          => '512m',
   env                   => [ "JVM_MAX_THREAD_STACK_SIZE=2048k" ],
   health_check_interval => 30,
 }
